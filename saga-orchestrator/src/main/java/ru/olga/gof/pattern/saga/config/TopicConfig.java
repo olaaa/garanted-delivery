62060073af139e2c43108ecc5531a76f44a535ee
Ola <lelagorizont@yandex.ru>
2026-04-12 04:11:08 +0400

    small fix

--- saga-orchestrator/src/main/java/ru/olga/gof/pattern/saga/config/TopicConfig.java
+++ saga-orchestrator/src/main/java/ru/olga/gof/pattern/saga/config/TopicConfig.java
@@ -11,42 +11,38 @@ package ru.olga.gof.pattern.saga.config;
  * - Events: публикуются сервисами после выполнения действий
  * - Commands: отправляются оркестратором для выполнения действий
  */
-public class TopicConfig {
+public final class TopicConfig {
 
-    // Tenant и namespace для изоляции
-    public static final String TENANT = "public";
-    public static final String NAMESPACE = "default";
+    private TopicConfig() {}
+
+    private static final String TENANT = "public";
+    private static final String NAMESPACE = "default";
+
+    private static String topic(String name) {
+        return "persistent://%s/%s/%s".formatted(TENANT, NAMESPACE, name);
+    }
 
     // События от Order Service
-    public static final String ORDER_CREATED_TOPIC =
-        String.format("persistent://%s/%s/order-created", TENANT, NAMESPACE);
+    public static final String ORDER_CREATED_TOPIC = topic("order-created");
 
     // Команды для Payment Service
-    public static final String PROCESS_PAYMENT_TOPIC =
-        String.format("persistent://%s/%s/process-payment", TENANT, NAMESPACE);
-    public static final String REFUND_PAYMENT_TOPIC =
-        String.format("persistent://%s/%s/refund-payment", TENANT, NAMESPACE);
+    public static final String PROCESS_PAYMENT_TOPIC = topic("process-payment");
+    public static final String REFUND_PAYMENT_TOPIC  = topic("refund-payment");
 
     // События от Payment Service
-    public static final String PAYMENT_PROCESSED_TOPIC =
-        String.format("persistent://%s/%s/payment-processed", TENANT, NAMESPACE);
-    public static final String PAYMENT_FAILED_TOPIC =
-        String.format("persistent://%s/%s/payment-failed", TENANT, NAMESPACE);
+    public static final String PAYMENT_PROCESSED_TOPIC = topic("payment-processed");
+    public static final String PAYMENT_FAILED_TOPIC    = topic("payment-failed");
 
     // Команды для Inventory Service
-    public static final String RESERVE_INVENTORY_TOPIC =
-        String.format("persistent://%s/%s/reserve-inventory", TENANT, NAMESPACE);
-    public static final String RELEASE_INVENTORY_TOPIC =
-        String.format("persistent://%s/%s/release-inventory", TENANT, NAMESPACE);
+    public static final String RESERVE_INVENTORY_TOPIC = topic("reserve-inventory");
+    public static final String RELEASE_INVENTORY_TOPIC = topic("release-inventory");
 
     // События от Inventory Service
-    public static final String INVENTORY_RESERVED_TOPIC =
-        String.format("persistent://%s/%s/inventory-reserved", TENANT, NAMESPACE);
-    public static final String INVENTORY_RESERVATION_FAILED_TOPIC =
-        String.format("persistent://%s/%s/inventory-reservation-failed", TENANT, NAMESPACE);
+    public static final String INVENTORY_RESERVED_TOPIC             = topic("inventory-reserved");
+    public static final String INVENTORY_RESERVATION_FAILED_TOPIC   = topic("inventory-reservation-failed");
 
     // Subscription names для consumers
     public static final String SAGA_ORCHESTRATOR_SUBSCRIPTION = "saga-orchestrator-sub";
-    public static final String PAYMENT_SERVICE_SUBSCRIPTION = "payment-service-sub";
+    public static final String PAYMENT_SERVICE_SUBSCRIPTION   = "payment-service-sub";
     public static final String INVENTORY_SERVICE_SUBSCRIPTION = "inventory-service-sub";
 }
